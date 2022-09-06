fdc6962282d555c2b2730bd4fb11da1bd69f0de3
Ernane Domingues <ernanedomingues@live.com>
2022-09-06 12:32:50 -0300

    Add files

---
+++ jogoteca.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def ola():
+    jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+    jogo2 = Jogo('Skyrim', 'Aventura', 'PC')
+    jogo3 = Jogo('Pokemon', 'Aventura/Fantasia', 'Nintendo')
+    jogo4 = Jogo('Mario', 'Aventura', 'Nintendo')
+    jogos = [jogo1, jogo2, jogo3, jogo4]
+    return render_template('lista.html', titulo='Jogos', jogos=jogos)
+
+
+app.run()
