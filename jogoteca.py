424aeb81a913c850331e3e641b95d5930c0606ec
Ernane Domingues <ernanedomingues@live.com>
2022-09-06 13:56:39 -0300

    Add css style and set up new game registration

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+
 
 class Jogo:
     def __init__(self, nome, categoria, console):
@@ -7,20 +8,33 @@ class Jogo:
         self.console = console
 
 
+jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+jogo2 = Jogo('Skyrim', 'Aventura', 'PC')
+jogo3 = Jogo('Pokemon', 'Aventura/Fantasia', 'Nintendo')
+jogo4 = Jogo('Mario', 'Aventura', 'Nintendo')
+jogos = [jogo1, jogo2, jogo3, jogo4]
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def ola():
-    jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
-    jogo2 = Jogo('Skyrim', 'Aventura', 'PC')
-    jogo3 = Jogo('Pokemon', 'Aventura/Fantasia', 'Nintendo')
-    jogo4 = Jogo('Mario', 'Aventura', 'Nintendo')
-    jogos = [jogo1, jogo2, jogo3, jogo4]
+def index():
     return render_template('lista.html', titulo='Jogos', jogos=jogos)
+
 
 @app.route('/cadastro')
 def cadastro():
     return render_template('cadastro.html', titulo='Cadastro de jogos')
 
-app.run()
+
+@app.route('/criar', methods=['POST', ])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    jogos.append(jogo)
+    return redirect('/')
+
+
+app.run(debug=True)
